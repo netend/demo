2c6850263df35121a02aeed9c5d2fbd9e1a38d54
netend <49399275+netend@users.noreply.github.com>
2019-04-16 15:53:28 +0800

    Update vnc.sh

--- sh/conf/vnc/vnc.sh
+++ sh/conf/vnc/vnc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 vnc_pass=$(wget https://github.com/netend/demo/raw/master/sh/conf/vnc/vncpasswd.conf -q -O-)
+vnc_start=$(wget https://github.com/netend/demo/raw/master/sh/conf/vnc/vnc-start -q -O-)
 apt-get install -y xfce4 xfce4-goodies gnome-icon-theme tightvncserver  iceweasel   websockify novnc  ttf-wqy-zenhei  #ttf-arphic-uming
 apt install expect -y
 wget http://download.daili.cf/52/zip/noVNC.zip
@@ -11,6 +12,13 @@ ln -s /var/noVNC/utils/launch.sh  /usr/local/bin/novnc
 cat <<EOFF >> /usr/local/bin/vncpass
 $vnc_pass
 EOFF
+chmod 755  /usr/local/bin/vncpass
+##################################
+cat <<EOF >> /usr/local/bin/vncstart
+$vnc_start
+EOF
+chmod 755 /usr/local/bin/vncstart
+
 sudo bash /usr/local/bin/vncpass
 vncserver
 sudo nohup novnc --vnc localhost:5901 >/dev/null&
