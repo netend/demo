bd5eb1de97020988ec3410b66ce58b32ad76d891
netend <49399275+netend@users.noreply.github.com>
2019-05-05 19:28:33 +0800

    Update netdata.sh

--- sh/conf/52/netdata.sh
+++ sh/conf/52/netdata.sh
@@ -2,7 +2,7 @@
 
 sudo rm -rf /usr/sbin/netdata
 sudo rm -rf /opt/netdata
-bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --non-interactive
+sudo bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --non-interactive
 
 echo "----------------"
 exit 0
